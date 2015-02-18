908174120924c16f89b8cecfcc814d351091533f
Brigid Prinsloo <b.i.prinsloo@gmail.com>
2015-02-18 12:00:57 +0200

    Complete Seed Data assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
 require 'faker'
- 
+
+ # Create a post with a unique title and body
+ Post.create!(title:"This is a unique title",body:"This is a unique body")
+
+ # Create a unique comment 
+ post=Post.last
+ post.comments.create(body:"This is a unique comment")
+
  # Create Posts
  50.times do
    Post.create!(
