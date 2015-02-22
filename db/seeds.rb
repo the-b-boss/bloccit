554425b6da274030867c0ac96f61a1672a125962
Brigid Prinsloo <b.i.prinsloo@gmail.com>
2015-02-22 14:47:10 +0200

    Trying to complete seed data assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,17 @@
 require 'faker'
 
- # Create a post with a unique title and body
- Post.create!(title:"This is a unique title",body:"This is a unique body")
-
  # Create a unique comment 
  post=Post.last
  post.comments.create(body:"This is a unique comment")
 
  # Create Posts
  50.times do
-   Post.create!(
-     title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
-   )
- end
+  newtitle = Faker::Lorem.sentence
+  newbody = Faker::Lorem.paragraph
+    Unless Post.exists?(:title => newtitle, :body => newbody)
+      Post.create!(:title => newtitle, :body => newbody)
+    end
+  end
  posts = Post.all
  
  # Create Comments
