0739a776b5bee183718779266c4503d7843510e4
Brigid Prinsloo <b.i.prinsloo@gmail.com>
2015-04-04 11:25:07 +0200

    Associations users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,25 @@
 require 'faker'
  
+# Create Users
+ 5.times do
+   user = User.new(
+     name:     Faker::Name.name,
+     email:    Faker::Internet.email,
+     password: Faker::Lorem.characters(10)
+   )
+   user.skip_confirmation!
+   user.save!
+ end
+ users = User.all
+ 
+ # Note: by calling `User.new` instead of `create`,
+ # we create an instance of User which isn't immediately saved to the database.
+ 
+ # The `skip_confirmation!` method sets the `confirmed_at` attribute
+ # to avoid triggering an confirmation email when the User is saved.
+ 
+ # The `save` method then saves this User to the database.
+
  # Create Posts
  50.times do
    Post.create!(
@@ -16,7 +36,15 @@ require 'faker'
      body: Faker::Lorem.paragraph
    )
  end
+
+ user = User.first
+ user.skip_reconfirmation!
+ user.update_attributes!(
+   email: 'b.i.prinsloo@gmail.com',
+   password: 'Perfect10'
+ )
  
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
