520eeadd490a63ad156e0be263fcddc122d4bb2d
Brigid Prinsloo <b.i.prinsloo@gmail.com>
2015-04-18 09:45:44 +0200

    Refactoring posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(post_params)
       authorize @post
      if @post.save
        flash[:notice] = "Post was saved."
@@ -32,7 +32,7 @@ class PostsController < ApplicationController
 
   def update
     @topic = Topic.find(params[:topic_id])
-     @post = Post.find(params[:id])
+     @post = current_user.posts.build(post_params)
       authorize @post
      if @post.update_attributes(params.require(:post).permit(:title, :body))
        flash[:notice] = "Post was updated."
@@ -42,5 +42,11 @@ class PostsController < ApplicationController
        render :edit
      end
    end
+
+   private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
    
 end
