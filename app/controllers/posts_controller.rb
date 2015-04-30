ab21bf993032177ec1fe82234f56c3ac1c762aab
Brigid Prinsloo <b.i.prinsloo@gmail.com>
2015-04-30 09:35:00 +0200

    Post markdown assignment completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,21 +45,6 @@ class PostsController < ApplicationController
      end
    end
 
-  def markdown_title(markdown)
-    render_as_markdown(@post.title)
-  end
-
-  def markdown_body(markdown)
-    render_as_markdown(@post.body)
-  end
-
-  def render_as_markdown(markdown)
-    renderer = Redcarpet::Render::HTML.new
-    extensions = {fenced_code_blocks: true}
-    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-    (redcarpet.render markdown).html_safe
-  end
-
   private
 
   def post_params
