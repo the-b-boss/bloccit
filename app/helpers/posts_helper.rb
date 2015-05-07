b63b195a567a55352043e6836033be579754bf86
Brigid Prinsloo <b.i.prinsloo@gmail.com>
2015-05-07 11:07:05 +0200

    Post markdown assignment completed

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,16 +1,3 @@
 module PostsHelper
-  def markdown_title(markdown)
-    render_as_markdown(@post.title)
-  end
 
-  def markdown_body(markdown)
-    render_as_markdown(@post.body)
-  end
-
-  def render_as_markdown(markdown)
-    renderer = Redcarpet::Render::HTML.new
-    extensions = {fenced_code_blocks: true}
-    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-    (redcarpet.render markdown).html_safe
-  end
 end
