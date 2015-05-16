eb688e899ee0f42993c4b291bdbd97f750da565c
Brigid Prinsloo <b.i.prinsloo@gmail.com>
2015-05-16 16:10:14 +0200

    Voting checkpoint WIP

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,18 @@ class Post < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
 
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    votes.sum(:value)
+  end
+
   default_scope { order('created_at DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :topics do
      resources :posts, except: [:index] do
         resources :comments, only: [:create, :destroy]
+
+          post '/up-vote' => 'votes#up_vote', as: :up_vote
+          post '/down-vote' => 'votes#down_vote', as: :down_vote
       end
    end
   get 'about' =>'welcome#about'
