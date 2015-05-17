805feb1df2c4d725573308e04c9d4e5a92da6c1c
Brigid Prinsloo <b.i.prinsloo@gmail.com>
2015-05-17 12:07:42 +0200

    Updated master with voting checkpoint typo

--- app/models/post.rb
+++ app/models/post.rb
@@ -26,10 +26,10 @@ class Post < ActiveRecord::Base
   end
 
   def update_rank
-    age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
-    new_rank = points + age_in_days
-
-    update_attribute(:rank, new_rank)
+     age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
+     new_rank = points + age_in_days
+ 
+     update_attribute(:rank, new_rank)
   end
   
 end
