3346f1b9216abf08b8d3b45e15b739f8276caba9
Brigid Prinsloo <b.i.prinsloo@gmail.com>
2015-05-23 14:25:09 +0200

    Favoriting Assignment WIP

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,28 @@ require 'rails_helper'
 describe User do 
 
   include TestFactories
+  include Devise::TestHelpers
 
   describe "#favorited(post)" do
-    xit "returns 'nil' if the user has not favorited the post" do 
+
+    before do 
+      @post = associated_post
+      @user = authenticated_user
+      sign_in @user
     end
 
-    xit "returns the appropriate favorite if it exists" do 
+    xit "returns 'nil' if the user has not favorited the post" do 
+      expect( @user.favorites.find_by_post_id(@post.id) ).to be_nil
     end
+
+    it "returns the appropriate favorite if it exists" do 
+      favorite = @user.favorites.where(post: @post).create
+      expect( @user.favorites.find_by_post_id(@post.id) ).not_to be_nil
+    end
+
+    xit "returns 'nil' if the user has favorited another post" do 
+      expect( )
+    end
+
   end
 end
