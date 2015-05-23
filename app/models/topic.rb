50a01b3e1336b384ea5f52e701a6994d4498cdde
Brigid Prinsloo <b.i.prinsloo@gmail.com>
2015-05-23 14:50:28 +0200

    Private topics checkpoint

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,4 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy 
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
 end
--- app/policies/topic_policy.rb
+++ app/policies/topic_policy.rb
@@ -12,4 +12,9 @@ class TopicPolicy < ApplicationPolicy
     create?
   end
 
+  def show?
+    record.public? || user.present?
+  end
+
+
 end
