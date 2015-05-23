469b08cc50c28259bde6fbb61793c2a1f6c20aea
Brigid Prinsloo <b.i.prinsloo@gmail.com>
2015-05-23 10:55:24 +0200

    Intro to Rspec assignment updated to include second test

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
-
 describe Vote do
   describe "validations" do
 
     describe "value validation" do
-
-      before do 
-        @post = Post.create(title: 'Post title', body: 'Post bodies must be pretty long.')
-        @vote = @post.votes.create(value: '3')
-      end
-
       it "only allows -1 or 1 as values" do
-        expect(@vote.valid?).to eq(false)
+
+        up_vote = Vote.new(value: 1)
+        expect( up_vote.valid? ).to eq(true)
+
+        down_vote = Vote.new(value: -1)
+        expect( down_vote.valid? ).to eq(true)
+
+        invalid_vote = Vote.new(value: 3)
+        expect( invalid_vote.valid? ).to eq(false)
       end
     end
   end
