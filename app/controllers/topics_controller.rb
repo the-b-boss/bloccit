052a2617ea8de0199bf8bf7b20910cc8cd8efce9
Brigid Prinsloo <b.i.prinsloo@gmail.com>
2015-06-02 17:15:43 +0200

    Final private topics assignment

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,6 @@
 class TopicsController < ApplicationController
   def index
     @topics = Topic.visible_to(current_user).paginate(page: params[:page], per_page: 10)
-    authorize @topic
   end
 
   def new
