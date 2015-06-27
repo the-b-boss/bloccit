dea35eeef309415d1b8a33696e124f62260ac785
Brigid Prinsloo <b.i.prinsloo@gmail.com>
2015-06-27 13:22:34 +0200

    Creating with Ajax checkpoint complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,30 @@
 class CommentsController < ApplicationController
   
   def create
-    # raise params.inspect
-#    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
-    @topic = @post.topic
-
-    @comment = current_user.comments.build(comment_params)
-
+    @comments = @post.comments
+#    @topic = @post.topic
+    @comment = current_user.comments.build( comment_params )
+    @comment.post = @post
+    @new_comment = Comment.new 
     authorize @comment
-
-    @comment.post_id = @post.id
+#    @comment.post_id = @post.id
      
      if @comment.save
        flash[:notice] = "Comment was saved."
-       redirect_to [@topic, @post]
+       # redirect_to [@topic, @post]
        # redirect_to topic_post_path(1, 54)
        # redirect_to /topics/@topic.id/posts/@post.id
        # /topics/1/posts/54
      else
        flash[:error] = "There was an error:  #{@comment.errors.full_messages.to_sentence}."
-       redirect_to [@topic, @post]
+       # redirect_to [@topic, @post]
      end
 
+     respond_to do |format| 
+      format.html
+      format.js
+    end
   end
 
   def destroy
@@ -51,7 +53,8 @@ class CommentsController < ApplicationController
     end
    end
 
-
+  private
+  
   def comment_params
     params.require(:comment).permit(:body)
   end
